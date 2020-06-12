1fb2d6267475cde740ef4a63c3ed5bc3594c6f4b
Rodrigo da Silva Santos <rodrigo.santos@monde.com.br>
2020-06-12 18:32:42 -0300

    Frontend

---
+++ src/components/home/Home.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import Main from '../template/Main'
+
+export default props =>
+    <Main icon="home" title="Inicio"
+        subtitle="Segundo Projeto do capitulo de React" >
+        <div className="display-4">
+            Bem vindo
+        </div>
+        <hr />
+        <p className="mb-0">
+            Sistema de exemplo de costrução de cadastro
+        </p>
+    </Main>
